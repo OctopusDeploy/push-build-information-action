9bb89869820c0acc72e656a76aaf3114e21584e6
hnrkndrssn <henrik@henrikandersson.org>
2022-06-14 09:57:01 +1000

    chore(tests): move some code around

--- src/test-setup.ts
+++ src/test-setup.ts
@@ -3,13 +3,13 @@ import * as tmp from 'tmp'
 tmp.setGracefulCleanup()
 const tmpdir = tmp.dirSync({template: 'push-build-information-XXXXXX'})
 process.env = Object.assign(process.env, {
-  INPUT_API_KEY: process.env['OCTOPUS_APIKEY'],
   INPUT_BRANCH: 'main',
   INPUT_DEBUG: false,
   INPUT_VERSION: '1.0.0',
-  INPUT_SERVER: process.env['OCTOPUS_URL'],
-  INPUT_PACKAGES: 'test.nupkg',
+  INPUT_PACKAGES: 'test',
   INPUT_OVERWRITE_MODE: 'OverwriteExisting',
+  OCTOPUS_CLI_API_KEY: process.env['OCTOPUS_APIKEY'],
+  OCTOPUS_CLI_SERVER: process.env['OCTOPUS_URL'],
   RUNNER_TEMP: tmpdir.name,
   RUNNER_TOOL_CACHE: tmpdir.name,
   GITHUB_ACTION: '1',
