cfab6c4b94387aa1a0ff5843319d9e29d66bd14a
Henrik Andersson <henrik@henrikandersson.org>
2022-06-27 12:04:54 +1000

    feat: Use the build information repository to push to Octopus (#56)
    Release-As: 1.0.0

--- src/input-parameters.ts
+++ src/input-parameters.ts
@@ -1,21 +1,23 @@
 import {getBooleanInput, getMultilineInput, getInput} from '@actions/core'
+import {OverwriteMode} from '@octopusdeploy/api-client'
 
 export interface InputParameters {
   branch?: string
   debug?: boolean
-  overwriteMode: string
+  overwriteMode: OverwriteMode
   packages: string[]
-  space: string
   version: string
 }
 
 export function get(): InputParameters {
+  const overwriteMode: OverwriteMode =
+    (OverwriteMode as any)[getInput('overwrite_mode')] || // eslint-disable-line @typescript-eslint/no-explicit-any
+    OverwriteMode.FailIfExists
   return {
     branch: getInput('branch'),
     debug: getBooleanInput('debug'),
-    overwriteMode: getInput('overwrite_mode'),
+    overwriteMode,
     packages: getMultilineInput('packages', {required: true}),
-    space: getInput('space', {required: true}),
     version: getInput('version', {required: true})
   }
 }
