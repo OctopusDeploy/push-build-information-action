ade3961b5a9c9df5fbb28d1ecf059d656e3f8826
hnrkndrssn <henrik@henrikandersson.org>
2022-06-27 11:43:06 +1000

    chore: use strongly typed parameter for overwrite mode

--- src/input-parameters.ts
+++ src/input-parameters.ts
@@ -1,18 +1,22 @@
 import {getBooleanInput, getMultilineInput, getInput} from '@actions/core'
+import {OverwriteMode} from '@octopusdeploy/api-client'
 
 export interface InputParameters {
   branch?: string
   debug?: boolean
-  overwriteMode: string
+  overwriteMode: OverwriteMode
   packages: string[]
   version: string
 }
 
 export function get(): InputParameters {
+  const overwriteMode: OverwriteMode =
+    (OverwriteMode as any)[getInput('overwrite_mode')] || // eslint-disable-line @typescript-eslint/no-explicit-any
+    OverwriteMode.FailIfExists
   return {
     branch: getInput('branch'),
     debug: getBooleanInput('debug'),
-    overwriteMode: getInput('overwrite_mode'),
+    overwriteMode,
     packages: getMultilineInput('packages', {required: true}),
     version: getInput('version', {required: true})
   }
