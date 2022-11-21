dab6b2b3a94f6fbc4baa6135a7e7032192b62ad0
Shannon Lewis <slewis74@gmail.com>
2022-11-21 12:40:21 +1000

    chore: test fix

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,6 +1,15 @@
 import { context } from '@actions/github'
+import { Client, ClientConfiguration, Logger } from '@octopusdeploy/api-client'
 import * as inputs from '../src/input-parameters'
 import * as octopus from '../src/push-build-information'
+import { CaptureOutput } from './test-helpers'
+
+const apiClientConfig: ClientConfiguration = {
+  userAgentApp: 'Test',
+  apiKey: process.env.OCTOPUS_TEST_API_KEY || 'API-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+  instanceURL: process.env.OCTOPUS_TEST_URL || 'http://localhost:8050',
+  space: process.env.OCTOPUS_TEST_SPACE || 'Default'
+}
 
 describe('inputs', () => {
   it('successfully get input parameters', async () => {
@@ -11,11 +20,35 @@ describe('inputs', () => {
 
 describe('build information', () => {
   it('successfully pushes build information', async () => {
+    const output = new CaptureOutput()
+
+    const logger: Logger = {
+      debug: message => output.debug(message),
+      info: message => output.info(message),
+      warn: message => output.warn(message),
+      error: (message, err) => {
+        if (err !== undefined) {
+          output.error(err.message)
+        } else {
+          output.error(message)
+        }
+      }
+    }
+
+    const config: ClientConfiguration = {
+      userAgentApp: 'Test',
+      instanceURL: apiClientConfig.instanceURL,
+      apiKey: apiClientConfig.apiKey,
+      logging: logger
+    }
+
+    const client = await Client.create(config)
+
     const inputParameters = inputs.get()
     const runId = context.runId
     if (runId === undefined) {
       throw new Error('GitHub run number is not defined')
     }
-    await octopus.pushBuildInformation(runId, inputParameters)
+    await octopus.pushBuildInformationFromInputs(client, runId, inputParameters)
   }, 100000)
 })
