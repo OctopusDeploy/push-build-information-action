8a28cca5a50186099c7451834c9cde97ed36ecd1
Geoff Lamrock <geoff.lamrock@gmail.com>
2023-08-23 16:25:47 +1000

    Remove access token from inputs and readme

--- src/input-parameters.ts
+++ src/input-parameters.ts
@@ -13,7 +13,7 @@ export interface InputParameters {
   server: string
   // Optional: One of API key or Access token is required, but you should use the OCTOPUS_API_KEY environment variable instead of this.
   apiKey?: string
-  // Optional: One of API key or Access token is required, but you should use the OCTOPUS_ACCESS_TOKEN environment variable instead of this.
+  // Optional: Access token can only be obtained from the OCTOPUS_ACCESS_TOKEN environment variable.
   accessToken?: string
   // Optional: You should prefer the OCTOPUS_SPACE environment variable
   space: string
@@ -31,7 +31,7 @@ export function get(isRetry: boolean): InputParameters {
   const parameters: InputParameters = {
     server: getInput('server') || process.env[EnvironmentVariables.URL] || '',
     apiKey: getInput('api_key') || process.env[EnvironmentVariables.ApiKey],
-    accessToken: getInput('access_token') || process.env[EnvironmentVariables.AccessToken],
+    accessToken: process.env[EnvironmentVariables.AccessToken],
     space: getInput('space') || process.env[EnvironmentVariables.Space] || '',
     packages: getMultilineInput('packages', { required: true }),
     version: getInput('version', { required: true }),
@@ -46,10 +46,11 @@ export function get(isRetry: boolean): InputParameters {
     )
   }
 
-  if (!parameters.apiKey && !parameters.accessToken)
+  if (!parameters.apiKey && !parameters.accessToken) {
     errors.push(
-      "One of API Key or Access Token are required, please specify explicitly through the 'api_key'/'access_token' inputs or set the OCTOPUS_API_KEY/OCTOPUS_ACCESS_TOKEN environment variable."
+      "The Octopus API Key is required, please specify explicitly through the 'api_key' input or set the OCTOPUS_API_KEY environment variable."
     )
+  }
 
   if (parameters.apiKey && parameters.accessToken) errors.push('Only one of API Key or Access Token can be supplied.')
 
